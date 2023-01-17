0f8b8f38c36087ffa4ad1d0492978fe762336356
lawlikeed <lawlikeed@gmail.com>
2023-01-17 15:58:48 -0600

    added more examples of print function

--- Lesson1/output.py
+++ Lesson1/output.py
@@ -5,5 +5,10 @@
 
 
 # Implementation
+print('Hello World!')
+print('To output text to the screen use the print() function!')
+print('Make sure to put your text inbetween single or double quotes!')
+print("Try for yourself!")
+print('Hello World!')
 name = input('') # Ask user what their name is inbetween the single quotes 
 print('') # Output the users name. You can either use print(f'...{name}...') or print(... + name + ...)
