88d3154c7da823e87992a1c8f85905c3513121ca
lawlikeed <lawlikeed@gmail.com>
2023-01-19 15:34:29 -0600

    challenge1

---
+++ Challenges/challenge1Fin.py
@@ -0,0 +1,6 @@
+pi = 3.1415926535
+heigt = float(input('Input the height of the cylinder: '))
+radius = float(input('Input the radius of the cylinder: '))
+print('...')
+volume = pi*radius*radius*heigt
+print(f'The volume of the cylinder is {volume:.5f}!')
